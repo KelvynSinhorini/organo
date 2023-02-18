c7a0eb9f9c71427c7f89226e36bedb725e2aefbc
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2023-02-18 12:15:55 -0300

    [FEAT] Adicionar inputs nome, cargo e imagem

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <InputText />
+      <InputText label="Nome" placeholder="Digite o seu nome" />
+      <InputText label="Cargo" placeholder="Digite o seu cargo" />
+      <InputText label="Imagem" placeholder="Digite o endereço da imagem" />
     </div>
   );
 }
