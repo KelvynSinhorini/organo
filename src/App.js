7eaa85a3f072c16469fc60a091938b74737d1a84
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2023-04-01 19:10:53 -0300

    [FEAT] Criar componente Form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Banner from './components/Banner/index.js';
-import InputText from './components/InputText/index.js';
+import Form from './components/Form/index.js';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <InputText label="Nome" placeholder="Digite o seu nome" />
-      <InputText label="Cargo" placeholder="Digite o seu cargo" />
-      <InputText label="Imagem" placeholder="Digite o endereço da imagem" />
+      <Form />
     </div>
   );
 }
